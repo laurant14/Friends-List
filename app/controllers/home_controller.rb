59bdc3ba501565ee8c57b92fd7f0954fd0772305
Laura <laurantrombetta@gmail.com>
2025-08-11 14:09:14 -0400

    design changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
-  def index
+  def index 
   end
 
   def about
+    @users = User.all # Makes my users available to be displayed in my Home webpage
   end
 end
+
